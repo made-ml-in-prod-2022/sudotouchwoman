5542e2980be2b2f87f10fb5034cde98c77ba158c
Nikita Teterin <nekitonum@gmail.com>
2022-05-14 14:11:43 +0300

    copy src from ml_project dir (to avoid errors during pickling

---
+++ online_inference/src/features/__init__.py
@@ -0,0 +1,9 @@
+from .extract import extract_feature_columns, extract_target, split_data
+from .preprocessing import Preprocessor
+
+__all__ = [
+    "extract_feature_columns",
+    "extract_target",
+    "split_data",
+    "Preprocessor",
+]
---
+++ online_inference/src/settings/params.py
@@ -0,0 +1,13 @@
+from .data_params import DatasetConfig, SplitConfig
+from .features_params import FeaturesConfig
+from .root_params import RootConfig, InfConfig
+from .training_params import EstimatorConfig
+
+__all__ = [
+    "DatasetConfig",
+    "SplitConfig",
+    "FeaturesConfig",
+    "RootConfig",
+    "InfConfig",
+    "EstimatorConfig"
+]
