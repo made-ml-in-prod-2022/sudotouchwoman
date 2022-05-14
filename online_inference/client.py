eebb81e72002fea00a59e3bdf9665f0d5c172a47
Nikita Teterin <nekitonum@gmail.com>
2022-05-14 19:55:08 +0300

    add more examples to payload, update path to data

--- online_inference/client.py
+++ online_inference/client.py
@@ -6,20 +6,23 @@ import click
 @click.command()
 @click.option("-p", "--port", default=5000)
 @click.option("-h", "--host", default="127.0.0.1")
-@click.option("-d", "--data", default="tmp/payload.json")
+@click.option("-d", "--data", default="data/payload.json")
 def main(port, host, data):
-    click.secho("Sample Client", fg="green")
+    click.secho("Runs testing client", fg="green")
     try:
         with open(data, "r") as f:
+            click.secho(f"Loads payload JSON from {data}")
             params = json.load(f)
             payload = {"payload": json.dumps(params)}
+            click.secho(payload, fg="white")
 
-            click.secho(params, fg="white")
         url = f"http://{host}:{port}/predict"
         click.secho("Connecting to " + url, fg="green")
+
         with requests.get(url=url, params=payload) as response:
             response.raise_for_status()
             click.secho(response.text, fg="white")
+
     except Exception as e:
         click.secho(f"Exception: {e}", fg="red")
     click.secho("Done", fg="green")
