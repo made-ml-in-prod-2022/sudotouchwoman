38203df144efad9b6178aaa22c07b1e65d115483
Nikita Teterin <nekitonum@gmail.com>
2022-06-10 22:03:08 +0300

    update script to run image with custom tag

--- online_inference/scripts/docker-run.sh
+++ online_inference/scripts/docker-run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-docker run --env-file env/dev.env -p 5005:5000 sudotouchwoman/wbcd-online-inference:v1
+[ "$#" -eq 1 ] && TAG=$1 || TAG=v2
+docker run --env-file env/dev.env -p 5005:5000 sudotouchwoman/wbcd-online-inference:$TAG
